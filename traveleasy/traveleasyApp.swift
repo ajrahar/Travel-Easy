5489807adedfc29e38be6778c2df9f1b6b6f9925
ajrahar <miftahulfazir@gmail.com>
2026-02-28 05:36:35 +0700

    Initial Commit

---
+++ traveleasy/traveleasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  traveleasyApp.swift
+//  traveleasy
+//
+//  Created by Miftahul Fazi on 28/02/26.
+//
+
+import SwiftUI
+
+@main
+struct traveleasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ traveleasyTests/traveleasyTests.swift
@@ -0,0 +1,17 @@
+//
+//  traveleasyTests.swift
+//  traveleasyTests
+//
+//  Created by Miftahul Fazi on 28/02/26.
+//
+
+import Testing
+@testable import traveleasy
+
+struct traveleasyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
